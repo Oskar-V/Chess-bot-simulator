30f04a0d75ce64a1a41ac0c46a2a470c0b5f65a0
ktaal <karltaal@gmail.com>
2021-12-25 21:19:23 +0200

    improved piece click

--- src/components/Piece.vue
+++ src/components/Piece.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="piece" >
-    <img v-if="color === 'w' && type === 'b'" src="../assets/wb.svg" class="piece-icon" :class="[clickable ? 'clickable' : '', isSelected ? 'selected-piece': '']" @click="handlePieceClick"/>
-    <img v-if="color === 'w' && type === 'k'" src="../assets/wk.svg" class="piece-icon" :class="[clickable ? 'clickable' : '', isSelected ? 'selected-piece': '']" @click="handlePieceClick"/>
-    <img v-if="color === 'w' && type === 'n'" src="../assets/wn.svg" class="piece-icon" :class="[clickable ? 'clickable' : '', isSelected ? 'selected-piece': '']" @click="handlePieceClick"/>
-    <img v-if="color === 'w' && type === 'p'" src="../assets/wp.svg" class="piece-icon" :class="[clickable ? 'clickable' : '', isSelected ? 'selected-piece': '']" @click="handlePieceClick"/>
-    <img v-if="color === 'w' && type === 'q'" src="../assets/wq.svg" class="piece-icon" :class="[clickable ? 'clickable' : '', isSelected ? 'selected-piece': '']" @click="handlePieceClick"/>
-    <img v-if="color === 'w' && type === 'r'" src="../assets/wr.svg" class="piece-icon" :class="[clickable ? 'clickable' : '', isSelected ? 'selected-piece': '']" @click="handlePieceClick"/>
-    <img v-if="color === 'b' && type === 'b'" src="../assets/bb.svg" class="piece-icon" :class="[clickable ? 'clickable' : '', isSelected ? 'selected-piece': '']" @click="handlePieceClick"/>
-    <img v-if="color === 'b' && type === 'k'" src="../assets/bk.svg" class="piece-icon" :class="[clickable ? 'clickable' : '', isSelected ? 'selected-piece': '']" @click="handlePieceClick"/>
-    <img v-if="color === 'b' && type === 'n'" src="../assets/bn.svg" class="piece-icon" :class="[clickable ? 'clickable' : '', isSelected ? 'selected-piece': '']" @click="handlePieceClick"/>
-    <img v-if="color === 'b' && type === 'p'" src="../assets/bp.svg" class="piece-icon" :class="[clickable ? 'clickable' : '', isSelected ? 'selected-piece': '']" @click="handlePieceClick"/>
-    <img v-if="color === 'b' && type === 'q'" src="../assets/bq.svg" class="piece-icon" :class="[clickable ? 'clickable' : '', isSelected ? 'selected-piece': '']" @click="handlePieceClick"/>
-    <img v-if="color === 'b' && type === 'r'" src="../assets/br.svg" class="piece-icon" :class="[clickable ? 'clickable' : '', isSelected ? 'selected-piece': '']" @click="handlePieceClick"/>
+  <div class="piece" :class="clickable ? 'clickable' : ''" @click="handlePieceClick">
+    <img v-if="color === 'w' && type === 'b'" src="../assets/wb.svg" class="piece-icon" :class="[isSelected ? 'selected-piece': '']"/>
+    <img v-if="color === 'w' && type === 'k'" src="../assets/wk.svg" class="piece-icon" :class="[isSelected ? 'selected-piece': '']"/>
+    <img v-if="color === 'w' && type === 'n'" src="../assets/wn.svg" class="piece-icon" :class="[isSelected ? 'selected-piece': '']"/>
+    <img v-if="color === 'w' && type === 'p'" src="../assets/wp.svg" class="piece-icon" :class="[isSelected ? 'selected-piece': '']"/>
+    <img v-if="color === 'w' && type === 'q'" src="../assets/wq.svg" class="piece-icon" :class="[isSelected ? 'selected-piece': '']"/>
+    <img v-if="color === 'w' && type === 'r'" src="../assets/wr.svg" class="piece-icon" :class="[isSelected ? 'selected-piece': '']"/>
+    <img v-if="color === 'b' && type === 'b'" src="../assets/bb.svg" class="piece-icon" :class="[isSelected ? 'selected-piece': '']"/>
+    <img v-if="color === 'b' && type === 'k'" src="../assets/bk.svg" class="piece-icon" :class="[isSelected ? 'selected-piece': '']"/>
+    <img v-if="color === 'b' && type === 'n'" src="../assets/bn.svg" class="piece-icon" :class="[isSelected ? 'selected-piece': '']"/>
+    <img v-if="color === 'b' && type === 'p'" src="../assets/bp.svg" class="piece-icon" :class="[isSelected ? 'selected-piece': '']"/>
+    <img v-if="color === 'b' && type === 'q'" src="../assets/bq.svg" class="piece-icon" :class="[isSelected ? 'selected-piece': '']"/>
+    <img v-if="color === 'b' && type === 'r'" src="../assets/br.svg" class="piece-icon" :class="[isSelected ? 'selected-piece': '']"/>
   </div>
 </template>
 
